03a4e0adb76ee44866ff66f3c3edd71ecfae2b66
Laviz-pand <pandeylaviz@gmail.com>
2023-01-06 14:24:01 +0545

    [Update] Navbar init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
-import './App.css'
+import Navbar from "./Components/Navbar"
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <Navbar/>
   )
 }
 export default App
