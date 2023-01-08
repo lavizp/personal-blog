c4fa58f38782b424d4b3e5df15e134945f780e8e
Laviz-pand <pandeylaviz@gmail.com>
2023-01-08 22:52:43 +0545

    [Update] Footer Update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import Navbar from "./Components/Navbar"
 import MainArticle from "./Components/MainArticle"
 import RecentBlogs from "./Components/RecentBlogs"
+import Footer from "./Components/Footer"
 function App() {
   return (
     <>
       <Navbar/>
       <MainArticle/>
       <RecentBlogs/>
+      <Footer/>
     </>
   )
 }
---
+++ client/src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-secondary m-10 rounded-xl text-white pt-5 pb-5'>
+        <h3 className='text-center font-bold text-xl'>All Rights Reserved</h3>
+        <h5 className='text-center'>This webiste was designed by <a href='https://dribbble.com/shots/19338398-Blog-home-page-Untitled-UI' className='underline' target="_blank">Untiteled-UI</a> and developed by <a href='https://www.lavizpandey.com.np/' className='underline' target="_blank">Laviz Pandey</a>.</h5>
+        <h5 className='text-center'>Connect With Me</h5>
+    </div>
+  )
+}
