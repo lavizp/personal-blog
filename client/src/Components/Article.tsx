adb1c31f24ade078a41486c7a2aeda316e867f90
Laviz-pand <pandeylaviz@gmail.com>
2023-01-11 21:52:54 +0545

    [Fix] Image Stretch in Article

--- client/src/Components/Article.tsx
+++ client/src/Components/Article.tsx
@@ -7,10 +7,10 @@ const Article: React.FC<Props>=({article}) =>{
   return (
     <div className='h-[350px] w-[350px]  flex flex-col'>
         <div className='w-full h-full overflow-hidden pb-4'>
-            <img src={article.image} className="w-full h-full rounded-xl "></img>
+            <img src={article.image} className="w-full h-full rounded-xl object-cover"></img>
         </div>
         <h3 className='font-bold text-lg'>{article.title}</h3>
-        <h6 className='text-sm'>Untitled UI Icons are a clean, consistent, and neutral icon library crafted specifically for modern UI design.</h6>
+        <h6 className='text-sm'>{article.main.slice(0,100)}...</h6>
     </div>
   )
 }
