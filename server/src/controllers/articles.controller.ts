155c029eba2077b87dfd19f8e2e70a8d8949b375
Laviz-pand <pandeylaviz@gmail.com>
2023-01-18 21:26:12 +0545

    [Update] Dummy Data Update

--- server/src/controllers/articles.controller.ts
+++ server/src/controllers/articles.controller.ts
@@ -15,9 +15,9 @@ const dummy:ArticleDocument[] = [
     },
     {
         id: "2",
-        title: "string",
+        title: "Why Newton was a Ronaldo Fan",
         image: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        main: "string"
+        main: "Because According to Newtons Fourth Law of Thermodynamics, SUII"
     }
 ]
 export const getArticles = async(req: Request,res: Response) =>{
