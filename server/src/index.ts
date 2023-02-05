a2519c766a5ff9d8aa16cf5356f6a1eac5dae6b7
Laviz-pand <pandeylaviz@gmail.com>
2023-02-05 10:47:57 +0545

    [Add] Search Bar Server Side

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,7 @@ dotenv.config();
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 const port = process.env.PORT;
 app.listen(port, async() => {
--- server/src/routes/articles.ts
+++ server/src/routes/articles.ts
@@ -1,11 +1,13 @@
 import { Express}  from "express";
-import { getArticles, getMainArticle,getArticle, writeArticle } from "../controllers/articles.controller"
+import { getArticles, getMainArticle,getArticle, writeArticle, searchArticles } from "../controllers/articles.controller"
 
 const router =(app: Express)=>{
     app.get("/articles", getArticles)
     app.get("/articles/:id", getArticle)
     app.get("/main/article", getMainArticle)
     app.post("/writeArticle", writeArticle)
+    app.get("/search", searchArticles)
+
 }
 
 
