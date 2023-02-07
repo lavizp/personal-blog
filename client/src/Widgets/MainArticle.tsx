bcad69e6bf6f486f6415a902d2fd3abd2cd0442d
Laviz-pand <pandeylaviz@gmail.com>
2023-02-07 12:46:28 +0545

    [Fix] Main Article HTML Error Fixed

--- client/src/Widgets/MainArticle.tsx
+++ client/src/Widgets/MainArticle.tsx
@@ -13,6 +13,7 @@ import {TfiArrowCircleRight} from "react-icons/tfi"
       }
       getMain();
   },[])
+  if(!article) return <h1>Loading Article....</h1>
   return (
     
 
@@ -23,7 +24,7 @@ import {TfiArrowCircleRight} from "react-icons/tfi"
         <div className='w-4/5 absolute bottom-28 left-40 flex flex-col gap-5'>
             <h3 className=' text-white text-3xl'>Featured</h3>
             <h3 className=' text-white text-5xl font-bold mb-[-5px]'>{article?.title}</h3>
-            <h3 className=' text-white w-4/5'>{article?.main.slice(0,200)}</h3>
+            <h3 className=' text-white w-4/5' dangerouslySetInnerHTML={{__html: article.main.slice(0,200)}}></h3>
 
         </div>
         <div className='absolute bottom-60 right-24'>
