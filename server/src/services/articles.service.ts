b6e78101b175af9db2cb9b57af7fbab1d912ec9a
Laviz-pand <pandeylaviz@gmail.com>
2023-02-11 11:06:25 +0545

    [Fix] User Page Single Article Fix

--- server/src/services/articles.service.ts
+++ server/src/services/articles.service.ts
@@ -22,6 +22,7 @@ export const getUnvalidatedArticles = async()=>{
 
 export const getSingleArticle = async(id: string) =>{
     let article = await ArticleModel.findById(id)
-    console.log(article)
-    return article;
+    if(article?.isvalidated){
+        return article;
+    }
 }
